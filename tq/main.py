79d13e1f34b91a91d19215641fe40c6d16b9ecbf
freeMind <freemind@highvillage.de>
2014-12-08 00:22:23 +0100

    first working version

--- tq/main.py
+++ tq/main.py
@@ -1,9 +1,10 @@
 import argparse
+import sys
 import urllib.request
 import json
 
-
-class Stream(object):
+""" crap
+Class Stream(object):
     def __init__(self):
         pass
 
@@ -13,15 +14,32 @@ class Stream(object):
 
     def _getjson(self, url):
         self.json = json.loads(self._geturl(url).decode('utf8'))
-        return self.json
+        for results in self.json["featured"]:
+            return results["stream"]["channel"]["url"]
 
     def getstream(self, url):
-        print(self._getjson(url))
+        print(json.dumps(self._getjson(url), indent=4))
+"""
+
+
+def get_featured(url):
+    response = urllib.request.urlopen(url).read()
+    streams = json.loads(response.decode('utf8'))
+    for stream in streams["featured"]:
+        print(stream["stream"]["channel"]["name"], stream["stream"]["channel"]["url"])
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.parse_args()
-    url = "https://api.twitch.tv/kraken/streams/featured"
-    x = Stream()
-    x.getstream(url)
+    parser = argparse.ArgumentParser(prog='tq')
+    parser.add_argument("-f", "--featured", help="Get a list of featured streams", action="store_true")
+    parser.add_argument("-g", "--games", help="Get games by number of viewers", action="store_true")
+    parser.add_argument("-v", "--version", action="version", version="%(prog)s alpha version v0.1")
+    parser.add_argument("--game", help="Find games")
+    args = parser.parse_args()
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+
+    if args.featured:
+        get_featured("https://api.twitch.tv/kraken/streams/featured")
