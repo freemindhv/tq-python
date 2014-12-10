6ddf33e43ce2e6c3b76d00d0fbf55812d19361d5
freeMind <freemind@highvillage.de>
2014-12-10 21:39:46 +0100

    modified code for restructure

--- tq/lib/streamhandler.py
+++ tq/lib/streamhandler.py
@@ -1,1 +1,64 @@
-__author__ = 'freemind'
+import urllib.request
+import json
+
+
+class StreamURL:
+    def __init__(self):
+        self.apiurl = "https://api.twitch.tv/kraken"
+
+    def build_url(self, option):
+        options = {"featured": "/streams/featured",
+                   "top": "/games/top",
+                   "search": "/search/streams?q="
+        }
+        return self.apiurl + options[option]
+
+
+class StreamHandler:
+    def __init__(self):
+        self.url = None
+
+    def get_stream(self):
+        print("not implemented yet")
+
+    def get_json(self):
+        req = urllib.request.Request(self.url)
+        req.add_header("Accept", "application/vnd.twitchtv.v2")
+        response = urllib.request.urlopen(req).read()
+        content = json.loads(response.decode('utf8'))
+        return content
+
+
+class FeaturedStream(StreamHandler):
+    def __init__(self):
+        self.url = "https://api.twitch.tv/kraken/streams/featured"
+
+    def get_stream(self):
+        content = self.get_json()
+        for c in content["featured"]:
+            name = c["stream"]["channel"]["name"]
+            url = c["stream"]["channel"]["url"]
+            print("Channel: {:<20}{}".format(name, url))
+
+
+class TopGames(StreamHandler):
+    def __init__(self):
+        self.url = StreamURL().build_url("top")
+
+    def get_stream(self):
+        content = self.get_json()
+        for c in content["top"]:
+            print("{:<40} Viewers:{}".format(c["game"]["name"], c["viewers"]))
+
+
+class SearchGames(StreamHandler):
+    def __init__(self, query):
+        self.url = "https://api.twitch.tv/kraken/search/streams?q={}".format(
+            query)
+
+    def get_stream(self):
+        content = self.get_json()
+        for c in content["streams"]:
+            name = c["channel"]["name"]
+            url = c["channel"]["url"]
+            print("{:<20}{}".format(name, url))
--- tq/main.py
+++ tq/main.py
@@ -1,87 +1,19 @@
-import argparse
-import sys
-import urllib.request
-import json
-
-
-apiversion = 2
-apiurl = "https://api.twitch.tv/kraken"
-
-
-class StreamURL(object):
-    def __init__(self):
-        self.apiurl = "https://api.twitch.tv/kraken"
-
-    def build_url(self, option):
-        options = {"featured": "/streams/featured",
-                   "top": "/games/top",
-                   "search": "/search/streams?q="
-        }
-        return self.apiurl + options[option]
-
-
-def get_data(url):
-    req = urllib.request.Request(url)
-    req.add_header('Accept', 'application/vnd.twitchtv.v{}+json'.format(
-        apiversion))
-    response = urllib.request.urlopen(req).read()
-    content = json.loads(response.decode('utf8'))
-    return content
-
-
-def handle_featured(url):
-    content = get_data(url)
-    for c in content["featured"]:
-        name = c["stream"]["channel"]["name"]
-        url = c["stream"]["channel"]["url"]
-        print("Channel: {:<20}{}".format(name, url))
-
-
-def get_games(url):
-    content = get_data(url)
-    for c in content["top"]:
-        print("{:<40} Viewers:{}".format(c["game"]["name"], c["viewers"]))
-
-
-def get_streams(url):
-    content = get_data(url)
-    for c in content["streams"]:
-        print("{}".format(c))
+from lib.streamhandler import FeaturedStream, TopGames, SearchGames
+from lib.argparser import parser
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(prog='tq')
-    mainarguments = parser.add_argument_group("Main Arguments")
-    mainarguments.add_argument("-f", "--featured",
-                               help="Get a list of featured streams",
-                               action="store_true")
-    mainarguments.add_argument("-t", "--top",
-                               help="Get games by number of viewers",
-                               action="store_true")
-    mainarguments.add_argument("-v", "--version",
-                               action="version",
-                               version="%(prog)s alpha version v0.1")
-    mainarguments.add_argument("-s", "--streams",
-                               help="Find Sreams of <GAME>")
-    optionarguments = parser.add_argument_group("Option Arguments")
-    optionarguments.add_argument("--limit",
-                                 type=int,
-                                 default=25,
-                                 help="limit query to <limit> results")
     args = parser.parse_args()
-
-    if len(sys.argv) == 1:
-        parser.print_help()
-        sys.exit(1)
-
-    s = StreamURL()
-    urls = []
+    streams = []
 
     if args.featured:
-        urls.append(s.build_url("featured"))
-    if args.top:
-        urls.append(s.build_url("top"))
-    if args.streams:
-        urls.append(s.build_url("search") + args.streams)
+        streams.append(FeaturedStream())
 
-    print(urls)
+    if args.top:
+        streams.append(TopGames())
+
+    if args.streams:
+        streams.append(SearchGames(args.streams))
+
+    for stream in streams:
+        stream.get_stream()
