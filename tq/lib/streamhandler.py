2276ab3879b3f58f21964d0d50b5b68f98d522b1
freeMind <freemind@highvillage.de>
2014-12-11 21:40:56 +0100

    added fuction get_url() to dynamically build the API URL

--- tq/lib/streamhandler.py
+++ tq/lib/streamhandler.py
@@ -3,11 +3,12 @@ import json
 
 
 class StreamHandler:
-    def __init__(self):
+    def __init__(self, limit):
         self.url = None
+        self.limit = limit
 
     def get_stream(self):
-        raise Warning("Streamhandler has no get_stream function")
+        raise Warning("StreamHandler has no get_stream function")
 
     def get_json(self):
         req = urllib.request.Request(self.url)
@@ -16,12 +17,19 @@ class StreamHandler:
         content = json.loads(response.decode('utf8'))
         return content
 
+    def get_url(self, x):
+        querys = {"featured": "https://api.twitch.tv/kraken/streams/featured",
+                  "top": "https://api.twitch.tv/kraken/games/top",
+                  "search_games":
+                      "https://api.twitch.tv/kraken/search/streams?q="
+                  }
+        apistring = querys[x] + "?limit=" + str(self.limit)
+        return apistring
+
 
 class FeaturedStream(StreamHandler):
-    def __init__(self):
-        self.url = "https://api.twitch.tv/kraken/streams/featured"
-
     def get_stream(self):
+        self.url = self.get_url("featured")
         content = self.get_json()
         for c in content["featured"]:
             name = c["stream"]["channel"]["name"]
@@ -30,21 +38,16 @@ class FeaturedStream(StreamHandler):
 
 
 class TopGames(StreamHandler):
-    def __init__(self):
-        self.url = "https://api.twitch.tv/kraken/games/top"
-
     def get_stream(self):
+        self.url = self.get_url("top")
         content = self.get_json()
         for c in content["top"]:
             print("{:<40} Viewers:{}".format(c["game"]["name"], c["viewers"]))
 
 
 class SearchGames(StreamHandler):
-    def __init__(self, query):
-        self.url = "https://api.twitch.tv/kraken/search/streams?q={}".format(
-            query)
-
     def get_stream(self):
+        self.url = self.get_url("search_games")
         content = self.get_json()
         for c in content["streams"]:
             name = c["channel"]["name"]
