dee4426336d77129344ff0c5ea560356adea0109
d.greiner@highvillage.de <gOthik1804>
2015-01-16 14:17:07 +0100

    added -D flag for debug printing returns the value that was given by simply printing it. This will be rewritten as soon as a bug with whitespaces in URL's is fixed

--- tq/main.py
+++ tq/main.py
@@ -1,4 +1,4 @@
-from lib.query import FeaturedStream, TopGames, SearchGames
+from lib.query import FeaturedStream, TopGames, SearchGames, DebugPrint
 from lib.argparser import parser
 
 
@@ -14,6 +14,9 @@ if __name__ == '__main__':
 
     if args.streams:
         streams.append(SearchGames(args.streams, args.limit))
+
+    if args.debug:
+        streams.append(DebugPrint(args.debug))
 
     for stream in streams:
         stream.get_results()
